51318b6cfe3584f36bdabc6b763899413b07cf35
Hito3837 <htoshniwal3381@gmail.com>
2026-03-17 21:09:27 +0530

    UC4

---
+++ displaynamemultiple.java
@@ -0,0 +1,11 @@
+public class displaynamemultiple {
+    public static void main(String[] args) {
+        String name;
+        if (args.length > 0) {
+            name = String.join(", ", args);
+        } else {
+            name = "World";
+        }
+        System.out.println("Hello, " + name + "!");
+    }
+}
